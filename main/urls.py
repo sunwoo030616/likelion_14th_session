e98c4eee6c1c7aaafeb74860500e99eceb07ec66
lo2q <suhyeonkvvon@gmail.com>
2026-04-06 01:28:28 +0900

    ✨ Feat: BE 1주차 세션 초기세팅

--- main/urls.py
+++ main/urls.py
@@ -6,10 +6,4 @@ from .views import *
 app_name = 'main'
 urlpatterns = [
     path('', mainpage, name='mainpage'),
-    path('blog', blogpage, name='blogpage'),
-    path('new_blog', new_blog, name='new_blog'),
-    path('create', create, name='create'),
-    path('detail/<int:blog_id>', detail, name='detail'),
-    path('edit/<int:blog_id>', edit, name='edit'),
-    path('update/<int:blog_id>', update, name='update'),
 ]
